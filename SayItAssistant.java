44e074531bd5e0f24e8adecc3024f114a084ca54
Ye-Au <acyeung@ucsd.edu>
2023-05-06 15:08:58 -0700

    First Working History List

--- SayItAssistant.java
+++ SayItAssistant.java
@@ -6,8 +6,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
@@ -25,42 +27,51 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
-class Task extends JPanel {
+class HistoryQuestion extends JPanel {
 
   JLabel index;
-  JTextField taskName;
-  JButton doneButton;
+  JTextField question;
+  JTextField answer;
+  JButton selectButton; //remove or change to answer cutoff
 
   Color gray = new Color(218, 229, 234);
   Color green = new Color(188, 226, 158);
 
-  private boolean markedDone;
+  private boolean selected; //change to be if selected?
 
-  Task() {
-    this.setPreferredSize(new Dimension(400, 20)); // set size of task
+  HistoryQuestion() {
+    this.setPreferredSize(new Dimension(400, 40)); // set size of task
     this.setBackground(gray); // set background color of task
+    this.setLayout(new FlowLayout()); // set layout of task
 
-    this.setLayout(new BorderLayout()); // set layout of task
-
-    markedDone = false;
+    selected = false;
 
     index = new JLabel(""); // create index label
     index.setPreferredSize(new Dimension(20, 20)); // set size of index label
     index.setHorizontalAlignment(JLabel.CENTER); // set alignment of index label
-    this.add(index, BorderLayout.WEST); // add index label to task
+    this.add(index); // add index label to task
 
-    taskName = new JTextField(""); // create task name text field
-    taskName.setBorder(BorderFactory.createEmptyBorder()); // remove border of text field
-    taskName.setBackground(gray); // set background color of text field
+    question = new JTextField("Question: "); // create task name text field
+    question.setPreferredSize(new Dimension(100, 20));
+    //question.setBorder(BorderFactory.createEmptyBorder()); // remove border of text field
+    question.setBackground(gray); // set background color of text field
+    question.setEditable(false);
 
-    this.add(taskName, BorderLayout.CENTER);
+    answer = new JTextField("Answer: "); // create task name text field
+    answer.setPreferredSize(new Dimension(100, 20));
+    //question.setBorder(BorderFactory.createEmptyBorder()); // remove border of text field
+    answer.setBackground(green); // set background color of text field
+    answer.setEditable(false);
 
-    doneButton = new JButton("Done");
-    doneButton.setPreferredSize(new Dimension(80, 20));
-    doneButton.setBorder(BorderFactory.createEmptyBorder());
-    doneButton.setFocusPainted(false);
+    this.add(question);
+    this.add(answer);
 
-    this.add(doneButton, BorderLayout.EAST);
+    selectButton = new JButton("Select");
+    selectButton.setPreferredSize(new Dimension(80, 20));
+    selectButton.setBorder(BorderFactory.createEmptyBorder());
+    selectButton.setFocusPainted(false);
+
+    this.add(selectButton);
   }
 
   public void changeIndex(int num) {
@@ -69,32 +80,42 @@ class Task extends JPanel {
   }
 
   public JButton getDone() {
-    return doneButton;
+    return selectButton;
   }
 
   public boolean getState() {
-    return markedDone;
+    return selected;
   }
 
   public void changeState() {
-    if (markedDone) {
+    if (selected) {
       this.setBackground(gray);
-      taskName.setBackground(gray);
-      markedDone = false;
+      question.setBackground(gray);
+      selected = false;
     }
     else {
       this.setBackground(green);
-      taskName.setBackground(green);
-      markedDone = true;
+      question.setBackground(green);
+      selected = true;
     }
     revalidate();
   }
+
+  public void insertQuestion(String questionString) {
+      question.setText(question.getText() + questionString);
+      question.setCaretPosition(0);
+  }
+
+  public void insertAnswer(String ansString) {
+    question.setText(answer.getText() + ansString);
+}
 
 }
 
 class List extends JPanel {
 
   Color backgroundColor = new Color(240, 248, 255);
+  Boolean empty;
 
   List() {
     GridLayout layout = new GridLayout(10, 1);
@@ -103,22 +124,25 @@ class List extends JPanel {
     this.setLayout(layout); // 10 tasks
     this.setPreferredSize(new Dimension(400, 560));
     this.setBackground(backgroundColor);
+    empty = true;
+    //JTextField emptyNotif = new JTextField("No question history to display");
+    //this.add(emptyNotif);
   }
 
   public void updateNumbers() {
     Component[] listItems = this.getComponents();
 
     for (int i = 0; i < listItems.length; i++) {
-      if (listItems[i] instanceof Task) {
-        ((Task) listItems[i]).changeIndex(i + 1);
+      if (listItems[i] instanceof HistoryQuestion) {
+        ((HistoryQuestion) listItems[i]).changeIndex(i + 1);
       }
     }
   }
 
   public void removeCompletedTasks() {
     for (Component c : getComponents()) {
-      if (c instanceof Task) {
-        if (((Task) c).getState()) {
+      if (c instanceof HistoryQuestion) {
+        if (((HistoryQuestion) c).getState()) {
           remove(c); // remove the component
           updateNumbers(); // update the indexing of all items
         }
@@ -130,18 +154,18 @@ class List extends JPanel {
    * Loads tasks from a file called "tasks.txt"
    * @return an ArrayList of Task
    */
-  public ArrayList<Task> loadTasks() {
+  public ArrayList<HistoryQuestion> loadTasks() {
     // hint 1: use try-catch block
     // hint 2: use BufferedReader and FileReader
-    // hint 3: task.taskName.setText(line) sets the text of the task
-    ArrayList<Task> result = new ArrayList<>();
+    // hint 3: task.question.setText(line) sets the text of the task
+    ArrayList<HistoryQuestion> result = new ArrayList<>();
     try {
       FileReader file = new FileReader("/Users/rei_crzy/Documents/CSE 110/Lab 5/CSE110Lab5_/src/tasks.txt");
       BufferedReader br = new BufferedReader(file);
       String st;  
       while ((st = br.readLine()) != null) {
-        Task task = new Task();
-        task.taskName.setText(st);
+        HistoryQuestion task = new HistoryQuestion();
+        task.question.setText(st);
         result.add(task);
       }
       br.close();
@@ -167,8 +191,8 @@ class List extends JPanel {
       FileWriter file = new FileWriter("/Users/rei_crzy/Documents/CSE 110/Lab 5/CSE110Lab5_/src/tasks.txt");
       Component[] listOfSavedTasks = this.getComponents();
       for(int i = 0; i < listOfSavedTasks.length; i++) {
-        Task task = (Task) listOfSavedTasks[i];
-        file.write(task.taskName.getText() + '\n');
+        HistoryQuestion task = (HistoryQuestion) listOfSavedTasks[i];
+        file.write(task.question.getText() + '\n');
       }
       file.close();
     }
@@ -181,6 +205,7 @@ class List extends JPanel {
 class Footer extends JPanel {
 
   JButton questionButton;
+  JButton dummyAskQuestionButton;
 
   Color backgroundColor = new Color(240, 248, 255);
   Border emptyBorder = BorderFactory.createEmptyBorder();
@@ -191,14 +216,21 @@ class Footer extends JPanel {
     GridLayout layout = new GridLayout(1, 4);
     this.setLayout(layout);
 
-    questionButton = new JButton("Ask a Question"); // add task button
+    questionButton = new JButton("Ask a question"); // add task button
     questionButton.setFont(new Font("Sans-serif", Font.ITALIC, 10)); // set font
     this.add(questionButton); // add to footer
+
+    dummyAskQuestionButton = new JButton("Dummy Question Adder"); // add task button
+    dummyAskQuestionButton.setFont(new Font("Sans-serif", Font.ITALIC, 10)); // set font
+    this.add(dummyAskQuestionButton); // add to footer
 
   }
 
   public JButton getquestionButton() {
     return questionButton;
+  }
+  public JButton getdummyAskQuestionButton() {
+    return dummyAskQuestionButton;
   }
 }
 
@@ -224,10 +256,11 @@ class AppFrame extends JFrame {
   private List list;
 
   private JButton questionButton;
+  private JButton dummyAskQuestionButton;
 
   AppFrame() {
     this.revalidate();
-    this.setSize(400, 600); // 400 width and 600 height
+    this.setSize(800, 600); // 400 width and 600 height
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
     this.setVisible(true); // Make visible
 
@@ -237,9 +270,10 @@ class AppFrame extends JFrame {
 
     this.add(header, BorderLayout.NORTH); // Add title bar on top of the screen
     this.add(footer, BorderLayout.SOUTH); // Add footer on bottom of the screen
-    this.add(list, BorderLayout.CENTER); // Add list in middle of footer and title
+    this.add(list, BorderLayout.WEST); // Add list in middle of footer and title
 
     questionButton = footer.getquestionButton();
+    dummyAskQuestionButton = footer.getdummyAskQuestionButton();
 
     addListeners();
   }
@@ -253,8 +287,8 @@ class AppFrame extends JFrame {
         //   list.add(task); // Add new task to list
         //   list.updateNumbers(); // Updates the numbers of the tasks
 
-        //   JButton doneButton = task.getDone();
-        //   doneButton.addMouseListener(
+        //   JButton selectButton = task.getDone();
+        //   selectButton.addMouseListener(
         //     new MouseAdapter() {
         //       @override
         //       public void mousePressed(MouseEvent e) {
@@ -265,6 +299,23 @@ class AppFrame extends JFrame {
         //     }
         //   );
         // }
+      }
+    );
+
+    dummyAskQuestionButton.addActionListener(
+      (ActionEvent e) -> {
+          HistoryQuestion question = new HistoryQuestion();
+          list.add(question); // Add new task to list
+          question.insertQuestion("testing longer string bigger than field");
+          list.updateNumbers(); // Updates the numbers of the tasks
+          JButton selectButton = question.getDone();
+          selectButton.addActionListener(
+            (ActionEvent e2) -> {
+                question.changeState(); // Change color of task
+                list.updateNumbers(); // Updates the numbers of the tasks
+                revalidate(); // Updates the frame
+              }
+          );
       }
     );
   }
