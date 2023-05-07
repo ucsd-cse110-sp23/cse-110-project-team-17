a83d3ee5dec42ceeb333222423a7b7a95563d5fd
Ye-Au <acyeung@ucsd.edu>
2023-05-06 20:07:54 -0700

    Sidebar scrollable, infinte history

--- SayItAssistant.java
+++ SayItAssistant.java
@@ -20,17 +20,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 class HistoryQuestion extends JPanel {
 
   JLabel index;
-  JTextField question;
+  JTextArea question;
   JTextField answer;
   JButton selectButton; //remove or change to answer cutoff
 
@@ -40,9 +44,10 @@ class HistoryQuestion extends JPanel {
   private boolean selected; //change to be if selected?
 
   HistoryQuestion() {
-    this.setPreferredSize(new Dimension(400, 40)); // set size of task
+    this.setPreferredSize(new Dimension(400, 60)); // set size of task
     this.setBackground(gray); // set background color of task
     this.setLayout(new FlowLayout()); // set layout of task
+    this.setBorder(BorderFactory.createLineBorder(Color.black));;
 
     selected = false;
 
@@ -51,14 +56,14 @@ class HistoryQuestion extends JPanel {
     index.setHorizontalAlignment(JLabel.CENTER); // set alignment of index label
     this.add(index); // add index label to task
 
-    question = new JTextField("Question: "); // create task name text field
-    question.setPreferredSize(new Dimension(100, 20));
+    question = new JTextArea("Question: "); // create task name text field
+    question.setPreferredSize(new Dimension(300, 20));
     //question.setBorder(BorderFactory.createEmptyBorder()); // remove border of text field
     question.setBackground(gray); // set background color of text field
     question.setEditable(false);
 
     answer = new JTextField("Answer: "); // create task name text field
-    answer.setPreferredSize(new Dimension(100, 20));
+    answer.setPreferredSize(new Dimension(200, 20));
     //question.setBorder(BorderFactory.createEmptyBorder()); // remove border of text field
     answer.setBackground(green); // set background color of text field
     answer.setEditable(false);
@@ -73,6 +78,11 @@ class HistoryQuestion extends JPanel {
 
     this.add(selectButton);
   }
+
+  @Override
+    public Dimension getMaximumSize() {
+        return getPreferredSize();
+    }
 
   public void changeIndex(int num) {
     this.index.setText(num + ""); // num to String
@@ -116,87 +126,14 @@ class List extends JPanel {
 
   Color backgroundColor = new Color(240, 248, 255);
   Boolean empty;
+  JPanel innerList;
 
   List() {
-    GridLayout layout = new GridLayout(10, 1);
-    layout.setVgap(5); // Vertical gap
+    BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+    this.setLayout(layout);
+    setAlignmentY(TOP_ALIGNMENT);
 
-    this.setLayout(layout); // 10 tasks
-    this.setPreferredSize(new Dimension(400, 560));
-    this.setBackground(backgroundColor);
-    empty = true;
-  }
-
-  public void updateNumbers() {
-    Component[] listItems = this.getComponents();
-
-    for (int i = 0; i < listItems.length; i++) {
-      if (listItems[i] instanceof HistoryQuestion) {
-        ((HistoryQuestion) listItems[i]).changeIndex(i + 1);
-      }
-    }
-  }
-
-  public void removeCompletedTasks() {
-    for (Component c : getComponents()) {
-      if (c instanceof HistoryQuestion) {
-        if (((HistoryQuestion) c).getState()) {
-          remove(c); // remove the component
-          updateNumbers(); // update the indexing of all items
-        }
-      }
-    }
-  }
-
-  /**
-   * Loads tasks from a file called "tasks.txt"
-   * @return an ArrayList of Task
-   */
-  public ArrayList<HistoryQuestion> loadHistory() {
-    // hint 1: use try-catch block
-    // hint 2: use BufferedReader and FileReader
-    // hint 3: task.question.setText(line) sets the text of the task
-    ArrayList<HistoryQuestion> result = new ArrayList<>();
-    try {
-      FileReader file = new FileReader("History.txt");
-      BufferedReader br = new BufferedReader(file);
-      String st;  
-      while ((st = br.readLine()) != null) {
-        HistoryQuestion task = new HistoryQuestion();
-        task.question.setText(st);
-        result.add(task);
-      }
-      br.close();
-      file.close();
-    }
-    catch(Exception e) {
-      e.printStackTrace();
-    }
-    //System.out.println("loadTasks() not implemented");
-    updateNumbers();
-    revalidate();
-    return result;
-  }
-
-  /**
-   * Saves tasks to a file called "tasks.txt"
-   */
-  public void saveTasks() {
-    // hint 1: use try-catch block
-    // hint 2: use FileWriter
-    // hint 3 get list of Tasks using this.getComponents()
-    try {
-      FileWriter file = new FileWriter("/Users/rei_crzy/Documents/CSE 110/Lab 5/CSE110Lab5_/src/tasks.txt");
-      Component[] listOfSavedTasks = this.getComponents();
-      for(int i = 0; i < listOfSavedTasks.length; i++) {
-        HistoryQuestion task = (HistoryQuestion) listOfSavedTasks[i];
-        file.write(task.question.getText() + '\n');
-      }
-      file.close();
-    }
-    catch(Exception e) {
-      e.printStackTrace();
-    }
+    
   }
 }
 
@@ -265,10 +202,13 @@ class AppFrame extends JFrame {
     header = new Header();
     footer = new Footer();
     list = new List();
+    JScrollPane scrollPane = new JScrollPane(list);
+    scrollPane.setPreferredSize(new Dimension(420,400));
+    this.add(scrollPane, BorderLayout.WEST);
 
     this.add(header, BorderLayout.NORTH); // Add title bar on top of the screen
     this.add(footer, BorderLayout.SOUTH); // Add footer on bottom of the screen
-    this.add(list, BorderLayout.WEST); // Add list in middle of footer and title
+    //this.add(list, BorderLayout.WEST); // Add list in middle of footer and title
 
     questionButton = footer.getquestionButton();
     dummyAskQuestionButton = footer.getdummyAskQuestionButton();
@@ -304,13 +244,15 @@ class AppFrame extends JFrame {
       (ActionEvent e) -> {
           HistoryQuestion question = new HistoryQuestion();
           list.add(question); // Add new task to list
-          question.insertQuestion("testing longer string bigger than field");
-          list.updateNumbers(); // Updates the numbers of the tasks
+          question.insertQuestion("testing longer string bigger than field adwddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+          question.setMaximumSize(getPreferredSize());
+          //list.updateNumbers(); // Updates the numbers of the tasks
           JButton selectButton = question.getDone();
+          revalidate();
           selectButton.addActionListener(
             (ActionEvent e2) -> {
                 question.changeState(); // Change color of task
-                list.updateNumbers(); // Updates the numbers of the tasks
+                //list.updateNumbers(); // Updates the numbers of the tasks
                 revalidate(); // Updates the frame
               }
           );
