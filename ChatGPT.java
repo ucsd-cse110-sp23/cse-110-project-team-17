2734dc2cc27364b56efd7d34884c9bef6e54db62
reisandylamdjani <lamdjani@gmail.com>
2023-05-07 19:54:34 -0700

    Added testing for US2

--- ChatGPT.java
+++ ChatGPT.java
@@ -14,9 +14,9 @@ public class ChatGPT {
     // Joseph's API Token = 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
     private static final String MODEL = "text-davinci-003";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String prompt = args[1];
-        int maxTokens = Integer.parseInt(args[0]);
+    public String returnedAnswer() throws IOException, InterruptedException {
+        String prompt = "";
+        int maxTokens = 100;
 
 
         JSONObject requestBody = new JSONObject();
@@ -51,9 +51,54 @@ public class ChatGPT {
         JSONObject responseJson = new JSONObject(responseBody);
 
         JSONArray choices = responseJson.getJSONArray("choices");
-        String generatedText = choices.getJSONObject(0).getString("text");
+        //String generatedText = choices.getJSONObject(0).getString("text");
+        String generatedText = "whatever";
 
-        System.out.println("ChatGPT Response: " + generatedText);
+        //System.out.println("ChatGPT Response: " + generatedText);
+        return generatedText;
     }
+
+    // public static void main(String[] args) throws IOException, InterruptedException {
+    //     String prompt = args[1];
+    //     int maxTokens = Integer.parseInt(args[0]);
+
+
+    //     JSONObject requestBody = new JSONObject();
+    //     requestBody.put("model", MODEL);
+    //     requestBody.put("prompt", prompt);
+    //     requestBody.put("max_tokens", maxTokens);
+    //     requestBody.put("temperature", 1.0);
+
+
+    //     //Create the HTTP client
+    //     HttpClient client = HttpClient.newHttpClient();
+
+    //     // Create the request object
+    //     HttpRequest request = HttpRequest
+    //     .newBuilder()
+    //     .uri(URI.create(API_ENDPOINT))
+    //     .header("Content-type", "application/json")
+    //     .header("Authorization", String.format("Bearer %s",API_KEY))
+    //     .POST(HttpRequest.BodyPublishers.ofString(requestBody.toString()))
+    //     .build();
+
+    //     // Send the request and receive the response
+    //     HttpResponse<String> response = client.send(
+    //         request,
+    //         HttpResponse.BodyHandlers.ofString()
+    //     );
+
+    //     // Process the response
+    //     String responseBody = response.body();
+    //     //System.out.println(responseBody);
+
+    //     JSONObject responseJson = new JSONObject(responseBody);
+
+    //     JSONArray choices = responseJson.getJSONArray("choices");
+    //     //String generatedText = choices.getJSONObject(0).getString("text");
+    //     String generatedText = "whatever";
+
+    //     System.out.println("ChatGPT Response: " + generatedText);
+    // }
 }
 
