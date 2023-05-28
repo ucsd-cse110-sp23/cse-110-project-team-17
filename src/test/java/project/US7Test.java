778e23b9ce58a2b59c01bff055143d7b7a72832e
reisandylamdjani <lamdjani@gmail.com>
2023-05-27 23:48:06 -0700

    Merge main into MS2_US1 branch

---
+++ src/test/java/project/US7Test.java
@@ -0,0 +1,44 @@
+package project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import project.audio_handler.*;
+import project.chat_gpt.*;
+import project.question_handler.*;
+import project.gui.*;
+
+import java.io.*;
+
+public class US7Test {
+    @Test
+    void verifyUsername() {
+        IQuestionHandler qHandler = new MockQuestionHandler();
+        IChatGPT chatGPT = new MockChatGPT();
+        IAudioHandler audioHandler = new MockAudioHandler();
+        AppHandler testApp = new AppHandler(qHandler, chatGPT, audioHandler);
+        AppGUI appGUI = new AppGUI(testApp);
+        testApp.createGUI(appGUI);
+
+        LogInWindowGUI loginWindow = appGUI.getLoginWindow();
+        LogInWindowHandler loginHandler = testApp.getLogInWindowHandler();
+        assertFalse(loginHandler.createAccount("reisandy", "rei"));
+        assertTrue(loginHandler.verifyUsername("reisandy"));
+    }
+    
+    @Test
+    void verifyPassword() {
+        IQuestionHandler qHandler = new MockQuestionHandler();
+        IChatGPT chatGPT = new MockChatGPT();
+        IAudioHandler audioHandler = new MockAudioHandler();
+        AppHandler testApp = new AppHandler(qHandler, chatGPT, audioHandler);
+        AppGUI appGUI = new AppGUI(testApp);
+        testApp.createGUI(appGUI);
+
+        LogInWindowGUI loginWindow = appGUI.getLoginWindow();
+        LogInWindowHandler loginHandler = testApp.getLogInWindowHandler();
+        loginHandler.createAccount("joseph", "jo");
+        assertTrue(loginHandler.verifyPassword("joseph", "jo"));
+        assertFalse(loginHandler.createAccount("reisandy", "rei"));
+    }
+}
